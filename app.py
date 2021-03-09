ad26bc92fbf012b12c7a6af8b23994db8364179e
jingyi <zhao.elton@gmail.com>
2021-03-08 22:04:41 -0800

    added permission to the service

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import boto3
 
 app = Flask(__name__)
 
@@ -13,9 +14,17 @@ def hello(name):
     return 'Hello ' + name + '!'
 
 
-@app.route('/goodby/<name>')
-def hello(name):
-    return 'Hello ' + name + '!'
+@app.route('/hello/<phone_number>')
+def hello(phone_number):
+    sns = boto3.client('sns')
+    sns.publish(
+        PhoneNumber=phone_number,
+        Message=(
+            "Hello! Someone just wanted you to know that they're "
+            "thinking about you and you rock! - Sent from flaskapp"
+        )
+    )
+    return f'Thanks for stopping by, we are sending {phone_number} a nice text now!'
 
 
 if __name__ == '__main__':
